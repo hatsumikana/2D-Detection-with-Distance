e09d9d9355cf4869a84a6a7e1edb9a2f8120c859
Anirudh181001 <shrivijiani@gmail.com>
2022-06-10 11:47:22 +0800

    Code cleanup

--- measure_yolov5.py
+++ measure_yolov5.py
@@ -14,11 +14,6 @@ rs = RealsenseCamera()
 
 def draw_object_info(bgr_frame, depth_frame, obj_boxes, obj_classes , obj_centers):
         # loop through the detection
-        # print("object boxes: ", obj_boxes)
-        # print("object classes: ", obj_classes)
-        # print("object centers: ", obj_centers)
-        # print("bgr frame: ", bgr_frame.shape)
-        # print("depth frame: ", depth_frame.shape)
         for box, class_id, obj_center in zip(obj_boxes, obj_classes, obj_centers):
             x, y, x2, y2 = box
             
@@ -27,8 +22,6 @@ def draw_object_info(bgr_frame, depth_frame, obj_boxes, obj_classes , obj_center
             color = (int(color[0]), int(color[1]), int(color[2]))
 
             cx, cy = obj_center
-            # print(cx, cy)
-            # print(depth_frame.size)
             depth_mm = depth_frame[cy, cx] 
             
             cv2.line(bgr_frame, (cx, y), (cx, y2), color, 1)
@@ -54,7 +47,6 @@ while True:
     
     ret, bgr_frame, depth_frame = rs.get_frame_stream()
     bgr_src = bgr_frame.copy()
-    # depth_src = depth_frame.copy()
     total_fps.start()
     result = model(bgr_src)
     classes =  list(result.pandas().xyxy[0]["name"])
@@ -67,20 +59,18 @@ while True:
         # cv2.rectangle(bgr_src, (int(coord[0]),int(coord[1])), (int(coord[2]),int(coord[3])),(255, 255, 255),2)
         xCenter = int(coord[0]) + int((int(coord[2]) - int(coord[0]))/2)
         yCenter = int(coord[1]) + int((int(coord[3]) - int(coord[1]))/2)
-        # print("xcenter, ycenter: ",xCenter, yCenter)
+    
         centre_pts.append((xCenter, yCenter))
         obj_coordinates.append([int(coord[0]), int(coord[1]), int(coord[2]), int(coord[3])])
     
 	# Show depth info of the objects
-	# mrcnn.draw_object_info(bgr_frame, depth_frame)
     bgr = draw_object_info(bgr_frame, depth_frame, obj_coordinates, classes, centre_pts)
     total_fps.stop()
     
     cv2.putText(bgr, f"FPS: {total_fps.getFPS():.2f}", (7,40), cv2.FONT_HERSHEY_COMPLEX, 1.4, (100, 255, 0), 3, cv2.LINE_AA)
 
-# 	# cv2.imshow("depth frame", depth_frame)
+
     cv2.imshow("Bgr frame", bgr)
-# 	cv2.imshow("yolov5", yolov5)
 
     key = cv2.waitKey(1)
     if key == 27:
